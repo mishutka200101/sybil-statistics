ce7cf509f2621ba3f4c370a9f5249b9a3c6ca36e
mishutka200101 <mishutka200101@mail.ru>
2023-09-18 21:24:52 +0300

    edit main file

--- main.py
+++ main.py
@@ -27,24 +27,40 @@ def parse_transactions(transactions: list):
     Returns:
         list: Список статистики, включая общее количество транзакций и количество транзакций для каждого адреса получателя.
     """
-    contracts = {
-        '0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295': 'SyncSwap',
-        '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4': 'Maverick',
-        '0x943ac2310D9BC703d6AB5e5e76876e212100f894': 'Izumi',
-        '0xbE7D1FD1f6748bbDefC4fbaCafBb11C6Fc506d1d': 'SpaceFi',
-        '0x5673B6e6e51dE3479B8deB22dF46B12308db5E1e': 'Merkly'
-    }
+    SYNCSWAP = '0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295'
+    MAVERICK = '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4'
+    IZUMI = '0x943ac2310D9BC703d6AB5e5e76876e212100f894'
+    SPACEFI = '0xbE7D1FD1f6748bbDefC4fbaCafBb11C6Fc506d1d'
+    MERKLY = '0x5673B6e6e51dE3479B8deB22dF46B12308db5E1e'
 
-    txs = len(transactions)
-    counts = {contract: 0 for contract in contracts.values()}
+    txs, syncswap_count, maverick_count, izumi_count, spacefi_count, merkly_count = 0, 0, 0, 0, 0, 0
 
-    for transaction in transactions:
-        if transaction['status'] == 'verified':
-            to = transaction['to']
-            if to in addresses:
-                counts[contracts[to]] += 1
+    if transactions:
+        for transaction in transactions:
+            if transaction['status'] == 'verified':
+                txs += 1
+                to = transaction['to']
+                if to == SYNCSWAP:
+                    syncswap_count += 1
+                elif to == MAVERICK:
+                    maverick_count += 1
+                elif to == IZUMI:
+                    izumi_count += 1
+                elif to == SPACEFI:
+                    spacefi_count += 1
+                elif to == MERKLY:
+                    merkly_count += 1
 
-    return [txs] + list(counts.values())
+    result = [
+        txs,
+        syncswap_count,
+        maverick_count,
+        izumi_count,
+        spacefi_count,
+        merkly_count
+    ]
+
+    return result
 
 
 def parse_tokens(tokens: dict):
@@ -60,31 +76,33 @@ def parse_tokens(tokens: dict):
     def dec_to_int(balance: int, decimals: int):
         return balance / 10 ** decimals
 
-    ETH, USDC, USDT = 0, 0, 0
+    ETH = 0
+    USDC = 0
+    USDT = 0
 
-    for token in tokens.values():
-        balance = dec_to_int(balance=int(token['balance']), decimals=int(token['token']['decimals']))
-        symbol = token['token']['symbol']
+    if tokens:
+        for _ in tokens.keys():
+            token = tokens[_]
+            balance = dec_to_int(balance=int(token['balance']), decimals=int(token['token']['decimals']))
+            symbol = token['token']['symbol']
 
-        if symbol == 'ETH':
-            ETH = balance
-        elif symbol == 'USDC':
-            USDC = balance
-        elif symbol == 'USDT':
-            USDT = balance
+            if symbol == 'ETH':
+                ETH = balance
+            elif symbol == 'USDC':
+                USDC = balance
+            elif symbol == 'USDT':
+                USDT = balance
 
     return [ETH, USDC, USDT]
 
 
 async def get_transfers(session: ClientSession, transaction_id: str):
-    for _ in range(5):
-        try:
-            async with session.get(url=f'https://block-explorer-api.mainnet.zksync.io/transactions/{transaction_id}/transfers?limit=100&page=1') as res:
-                res_json = await res.json()
-                return res_json['items']
-        except Exception:
-            pass
-    return None
+    try:
+        async with session.get(url=f'https://block-explorer-api.mainnet.zksync.io/transactions/{transaction_id}/transfers?limit=100&page=1') as res:
+            res_json = await res.json()
+            return res_json.get('items', None)
+    except Exception:
+        pass
 
 
 async def get_tokens(session: ClientSession, address: str):
@@ -98,14 +116,12 @@ async def get_tokens(session: ClientSession, address: str):
     Returns:
         list: Список данных о токенах для адреса.
     """
-    for _ in range(5):
-        try:
-            async with session.get(url=f'https://block-explorer-api.mainnet.zksync.io/address/{address}') as res:
-                res_json = await res.json()
-                return res_json['balances']
-        except Exception as e:
-            print(f"Failed to fetch tokens for address {address}: {str(e)}")
-    return None
+    try:
+        async with session.get(url=f'https://block-explorer-api.mainnet.zksync.io/address/{address}') as res:
+            res_json = await res.json()
+            return res_json.get('balances', None)
+    except Exception as e:
+        print(f"Failed to fetch tokens for address {address}: {str(e)}")
 
 
 async def get_transactions(session: ClientSession, address: str):
@@ -125,14 +141,12 @@ async def get_transactions(session: ClientSession, address: str):
         'page': 1
     }
 
-    for _ in range(5):
-        try:
-            async with session.get(url=f'https://block-explorer-api.mainnet.zksync.io/transactions', params=params) as res:
-                res_json = await res.json()
-                return res_json['items']
-        except Exception as e:
-            print(f"Failed to fetch transactions for address {address}: {str(e)}")
-    return None
+    try:
+        async with session.get(url=f'https://block-explorer-api.mainnet.zksync.io/transactions', params=params) as res:
+            res_json = await res.json()
+            return res_json.get('items', None)
+    except Exception as e:
+        print(f"Failed to fetch transactions for address {address}: {str(e)}")
 
 
 async def run_workers(addresses: list):
@@ -158,10 +172,9 @@ async def run_workers(addresses: list):
     transactions = await asyncio.gather(*tasks_transactions)
 
     # task_transfers = []
-
+    #
     # for transaction in transactions:
-    #     task_transfers.append(
-    #         asyncio.ensure_future(get_transfers(session=session, transaction_id=transaction['hash'])))
+    #     task_transfers.append(asyncio.ensure_future(get_transfers(session=session, transaction_id=transaction['hash'])))
     #
     # transfers = await asyncio.gather(*task_transfers)
 
@@ -185,12 +198,10 @@ def run_all(addresses: list) -> list:
 
     for i, address in enumerate(addresses):
         ETH, USDC, USDT = parse_tokens(tokens[i])
-        txs, syncswap_count, maverick_count, izumi_count, spacefi_count, merkly_count = parse_transactions(
-            transactions[i])
+        txs, syncswap_count, maverick_count, izumi_count, spacefi_count, merkly_count = parse_transactions(transactions[i])
 
         result_list.append(
-            [address, ETH, USDC, USDT, txs, syncswap_count, maverick_count, izumi_count, spacefi_count,
-             merkly_count]
+            [address, ETH, USDC, USDT, txs, syncswap_count, maverick_count, izumi_count, spacefi_count, merkly_count]
         )
 
     return result_list
